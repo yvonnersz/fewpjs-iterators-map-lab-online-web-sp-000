75b7a45ae5f61a11c02bf0ab4926caef711aaa1b
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-07 05:09:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = function(tutorials) => {
+  return tutorials.map(tutorial => tutorial.upperCase)
 }
